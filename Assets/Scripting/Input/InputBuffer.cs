87b1007d21e2799c461481ae732c405719f7944e
goon <nathan.gooneratne@gmail.com>
2023-12-16 22:23:23 -0500

    setup controls + input wishlist
    I want input to be a priority-queue thing. Unfortunately, Unity doesn't have a recent version of .NET, so I had to port the source file into the project (fixing the various compiler errors).
    
    My general idea for input is to have it be essentially just a buffered list of input histories with "priority" inputs beating out other ones. A modular way to do this would be to have SOs arranging a list of input types into order to establish priorities. I can then use these priorities when adding elements to the PriorityQueue, since the queue separates the "value" from the "priority".
    
    Currently, InputBufferReader is a tentative class. Right now, it was used to debug PriorityQueue.

---
+++ Assets/Scripting/Input/InputBuffer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBuffer
+{
+    private List<InputType> m_buffer;
+
+    public InputBuffer()
+    {
+    }
+}
---
+++ Assets/Scripting/Input/InputType.cs
@@ -0,0 +1,9 @@
+public enum InputType
+{
+    Movement,
+    Jump,
+    Button1,
+    Button2,
+    Button3,
+    Button4
+}
